5cf93a4ea0a79a9fef6ed4899047c3c8ebde04c0
Yiran Jing <yiran@canva.com>
2022-12-03 11:51:07 +1100

    .

--- src/processors/javascript.ts
+++ src/processors/javascript.ts
@@ -56,7 +56,6 @@ export class JavascriptFileProcessor implements FileProcessor {
     files: ReadonlyArray<Path>,
     dependencyTree: DependencyTree,
   ): Promise<Set<Path>> {
-    // TODO: change
     const importedFiles = new Set<Path>();
     const referencedFiles = new Set<Path>(extractJsImports(contents));
     referencedFiles.forEach((referencedFile) => {
